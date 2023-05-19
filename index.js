803cf93c5bdf2aead44b127472a6717fef923039
Tofail Ahmed Sayem <atofail50@gmail.com>
2023-05-20 02:32:35 +0600

    addToy api added

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ async function run() {
 
             })
 
+            app.post('/addtoy',async(req,res)=>{
+                  const addToy=req.body
+                  console.log(addToy);
+                  const result=await toyCollection.insertOne(addToy)
+                  res.send(result)
+            })
+
             // Send a ping to confirm a successful connection
             await client.db("admin").command({ ping: 1 });
             console.log("Pinged your deployment. You successfully connected to MongoDB!");
